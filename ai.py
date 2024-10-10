c750644585f8e53627c8009f55c06b690bdf890d
JamesBullen <james.roberts.bullen@gmail.com>
2024-10-10 17:47:39 +0100

    added new fucntion

--- ai.py
+++ ai.py
@@ -1,5 +1,32 @@
 import math as m
+from gameLogic import previousBet, player, ai
 
+# How much risk the AI will take
+risk = 0.5
+
+def evaluatePreviousBet():
+    global previousBet
+    global player
+    global ai
+    global risk
+
+    # How many of my dice match the bet
+    matchingDice = 0
+    for i in range(len(ai.dice)):
+        if ai.dice[i] == previousBet[1]:
+            matchingDice += 1
+    
+    diceDifference = previousBet[0] - matchingDice
+    if diceDifference > 0:
+        # cal odds of them having the missing dice value
+        results = calculateOdds(len(player.dice), diceDifference)
+        if results < risk:
+            return False
+    
+    # Bet is valid
+    return True
+
+# Binomial distribution formula determining chance of atleast x dice of a value have been rolled
 def calculateOdds(unknownDice, successes):
     total = 0
     for target in range(successes, 5):
@@ -7,5 +34,3 @@ def calculateOdds(unknownDice, successes):
         total += m.factorial(unknownDice) / (m.factorial(unknownDice - target) * m.factorial(target)) * pow(0.25, target) * pow(0.75, unknownDice - target)
 
     return total
-
-print(calculateOdds(4,2))
