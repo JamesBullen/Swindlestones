c9e3b4877ed961a83c6635fbcc1224397953381c
JamesBullen <james.roberts.bullen@gmail.com>
2024-10-17 17:22:17 +0100

    implimenting ai

--- ai.py
+++ ai.py
@@ -10,13 +10,9 @@ def evaluatePreviousBet():
     global ai
     global risk
 
-    # How many of my dice match the bet
-    matchingDice = 0
-    for i in range(len(ai.dice)):
-        if ai.dice[i] == previousBet[1]:
-            matchingDice += 1
-    
-    diceDifference = previousBet[0] - matchingDice
+    # Difference in a dice value present verus betted
+    diceDifference = previousBet[0] - ai.countDice()[previousBet[1]]
+
     if diceDifference > 0:
         # cal odds of them having the missing dice value
         results = calculateOdds(len(player.dice), diceDifference)
@@ -25,6 +21,29 @@ def evaluatePreviousBet():
     
     # Bet is valid
     return True
+
+def decideBet():
+    global player
+    global ai
+    global previousBet
+
+    bestBet = [0,[0,0]]
+    for key, value in ai.countDice():
+        # Determines how many dice are needed for a valid bet
+        successesNeeded = previousBet[0] - value
+        if key <= previousBet[1]:
+            successesNeeded += 1
+
+        # Sets the bet odds to 100%
+        if successesNeeded <= 0:
+            bestBet = [1,[successesNeeded + previousBet[0], key]]
+            continue
+
+        betOdds = calculateOdds(player.diceRemaining, successesNeeded)
+        if betOdds > bestBet[0]:
+            bestBet = [betOdds, [successesNeeded + previousBet[0], key]]
+    
+    return bestBet[1]
 
 # Binomial distribution formula determining chance of atleast x dice of a value have been rolled
 def calculateOdds(unknownDice, successes):
